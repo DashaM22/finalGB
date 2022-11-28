daba3cd8d23cf95dbe64c058f871e3c2f729fac4
[Dasha Musikhina] <[dashamusihin@gmail.com]>
2022-11-28 13:09:26 +0300

    написала программу по заданным условиям из описания

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿string[] array = new string[] {"print", "new", "array", "123", "10"};
+string[] array1 = new string[array.Length];
+void ArraySelection(string[] array, string[] array1)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        array1[count] = array[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArraySelection(array, array1);
+PrintArray(array1);
